6c4949ef313b210a5a98b2782e7a9b9dd80255fb
Ben Deane <ben@elbeno.com>
2015-07-06 22:01:54 -0700

    Week 1 homework

--- week1/HW01.hs
+++ week1/HW01.hs
@@ -5,35 +5,34 @@ module HW01 where
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = undefined
+lastDigit = (`mod` 10)
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit = undefined
+dropLastDigit = (`div` 10)
 
 -- Exercise 2 -----------------------------------------
 
 toRevDigits :: Integer -> [Integer]
-toRevDigits = undefined
+toRevDigits = map lastDigit . takeWhile (>0) . iterate dropLastDigit
 
 -- Exercise 3 -----------------------------------------
 
 -- Double every second number in a list starting on the left.
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther = zipWith ($) (cycle [id, (*2)])
 
 -- Exercise 4 -----------------------------------------
 
 -- Calculate the sum of all the digits in every Integer.
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
-
+sumDigits = sum . concatMap toRevDigits
 
 -- Exercise 5 -----------------------------------------
 
 -- Validate a credit card number using the above functions.
 luhn :: Integer -> Bool
-luhn = undefined
+luhn = (==0) . lastDigit . sumDigits . doubleEveryOther . toRevDigits
 
 -- Exercise 6 -----------------------------------------
 
@@ -42,4 +41,25 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi n a b c
+  | n == 0 = []
+  | otherwise = hanoi (n-1) a c b
+                ++ [(a,b)]
+                ++ hanoi (n-1) c b a
+
+-- Exercise 7 -----------------------------------------
+
+-- Towers of Hanoi for four pegs
+
+-- 1: move n-k to d (the extra)
+-- 2: move the remaining k to b, using normal hanoi
+-- 3: move n-k back from d to b
+-- k is chosen such that n is the kth triangle number
+
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 n a b c d
+ | n == 0 = []
+ | otherwise  = hanoi4 (n-k) a d b c
+                ++ hanoi k a b c
+                ++ hanoi4 (n-k) d b a c
+  where k = head [x | x <- [1..], x * (x + 1) `div` 2 >= n]
--- week1/HW01Tests.hs
+++ week1/HW01Tests.hs
@@ -24,28 +24,63 @@ ex1Tests = [ Test "lastDigit test" testLastDigit
 
 -- Exercise 2 -----------------------------------------
 
+testToRevDigits :: (Integer, [Integer]) -> Bool
+testToRevDigits (n, ds) = toRevDigits n == ds
+
 ex2Tests :: [Test]
-ex2Tests = []
+ex2Tests = [ Test "toRevDigits test" testToRevDigits
+             [(1234, [4,3,2,1]), (0, []), (-17, [])]
+           ]
 
 -- Exercise 3 -----------------------------------------
 
+testDoubleEveryOther :: ([Integer], [Integer]) -> Bool
+testDoubleEveryOther (ns, ds) = doubleEveryOther ns == ds
+
 ex3Tests :: [Test]
-ex3Tests = []
+ex3Tests = [ Test "doubleEveryOther test" testDoubleEveryOther
+             [([4,9,5,5], [4,18,5,10]), ([0,0], [0,0])]
+           ]
 
 -- Exercise 4 -----------------------------------------
 
+testSumDigits :: ([Integer], Integer) -> Bool
+testSumDigits (ns, s) = sumDigits ns == s
+
 ex4Tests :: [Test]
-ex4Tests = []
+ex4Tests = [ Test "sumDigits test" testSumDigits
+             [([10,5,18,4], 19)]
+           ]
 
 -- Exercise 5 -----------------------------------------
 
+testLuhn :: (Integer, Bool) -> Bool
+testLuhn (n, b) = luhn n == b
+
 ex5Tests :: [Test]
-ex5Tests = []
+ex5Tests = [ Test "luhn test" testLuhn
+             [(5594589764218858, True), (1234567898765432, False)]
+           ]
 
 -- Exercise 6 -----------------------------------------
 
+testHanoi :: (Integer, Int) -> Bool
+testHanoi (n, l) = length (hanoi n "a" "b" "c") == l
+
 ex6Tests :: [Test]
-ex6Tests = []
+ex6Tests = [ Test "hanoi test" testHanoi
+             [(2,3), (4,15), (8,255)]
+           ]
+
+-- Exercise 7 -----------------------------------------
+
+testHanoi4 :: (Integer, Int) -> Bool
+testHanoi4 (n, l) = length (hanoi4 n "a" "b" "c" "d") == l
+
+ex7Tests :: [Test]
+ex7Tests = [ Test "hanoi4 test" testHanoi4
+             [(15,129)]
+           ]
 
 -- All Tests ------------------------------------------
 
@@ -56,4 +91,5 @@ allTests = concat [ ex1Tests
                   , ex4Tests
                   , ex5Tests
                   , ex6Tests
+                  , ex7Tests
                   ]
