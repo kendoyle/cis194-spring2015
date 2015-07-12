00e311c45647527315e9135914e57b5f6a1a3e7b
Ben Deane <ben@elbeno.com>
2015-07-12 10:03:59 -0700

    Week 5 homework (WIP)

--- week5/HW05.hs
+++ week5/HW05.hs
@@ -8,38 +8,68 @@ import System.Environment (getArgs)
 
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Map.Strict as Map
+import Data.Bits (xor)
+import Control.Applicative ((<*>))
+import Data.Maybe (fromMaybe)
 
 import Parser
 
 -- Exercise 1 -----------------------------------------
 
 getSecret :: FilePath -> FilePath -> IO ByteString
-getSecret = undefined
+getSecret originalFile modifiedFile = do
+  original <- BS.readFile originalFile
+  modified <- BS.readFile modifiedFile
+  return . BS.pack . filter (/= 0) $ BS.zipWith xor original modified
 
 -- Exercise 2 -----------------------------------------
 
 decryptWithKey :: ByteString -> FilePath -> IO ()
-decryptWithKey = undefined
+decryptWithKey k f = do
+  cipherText <- BS.readFile (f ++ ".enc")
+  let plainText = BS.pack $ BS.zipWith xor cipherText (BS.cycle k)
+  BS.writeFile f plainText
 
 -- Exercise 3 -----------------------------------------
 
 parseFile :: FromJSON a => FilePath -> IO (Maybe a)
-parseFile = undefined
+parseFile jsonFile = fmap decode (BS.readFile jsonFile)
 
 -- Exercise 4 -----------------------------------------
 
+badTs :: [TId] -> [Transaction] -> [Transaction]
+badTs tids = filter ((`elem` tids) . tid)
+
 getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
-getBadTs = undefined
+getBadTs victimsFile transactionsFile = do
+  victims <- parseFile victimsFile :: IO (Maybe [TId])
+  transactions <- parseFile transactionsFile :: IO (Maybe [Transaction])
+  return $ fmap badTs victims <*> transactions
 
 -- Exercise 5 -----------------------------------------
 
+insertWithOp :: (Transaction -> String) -> (Integer -> Integer -> Integer) ->
+                Transaction -> Map String Integer -> Map String Integer
+insertWithOp proj op t m = Map.insert (proj t) (v `op` amount t) m
+  where v = fromMaybe 0 (Map.lookup (proj t) m)
+
+insertTo :: Transaction -> Map String Integer -> Map String Integer
+insertTo = insertWithOp to (+)
+
+insertFrom :: Transaction -> Map String Integer -> Map String Integer
+insertFrom = insertWithOp from (-)
+
+insertTrans :: Transaction -> Map String Integer -> Map String Integer
+insertTrans t m = insertFrom t (insertTo t m)
+
 getFlow :: [Transaction] -> Map String Integer
-getFlow = undefined
+getFlow = foldr insertTrans Map.empty
 
 -- Exercise 6 -----------------------------------------
 
 getCriminal :: Map String Integer -> String
-getCriminal = undefined
+getCriminal m = fst $ Map.foldrWithKey f ("", 0) m
+  where f k a b = if a > snd b then (k,a) else b
 
 -- Exercise 7 -----------------------------------------
 
