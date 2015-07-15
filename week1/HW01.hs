c099aea1bc7f2bad35d823eb9fb073fb9c28fec7
Kenneth Doyle <ken@kennethdoyle.net>
2015-07-15 12:38:51 -0700

    Add kdoyle hw1 solutions.

--- week1/HW01.hs
+++ week1/HW01.hs
@@ -5,35 +5,39 @@ module HW01 where
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = undefined
+lastDigit x = mod x 10
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit = undefined
+dropLastDigit x = quot x 10
 
 -- Exercise 2 -----------------------------------------
 
 toRevDigits :: Integer -> [Integer]
-toRevDigits = undefined
+toRevDigits 0 = []
+toRevDigits x = [lastDigit x] ++ toRevDigits (dropLastDigit x)
 
 -- Exercise 3 -----------------------------------------
 
 -- Double every second number in a list starting on the left.
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther [] = []
+doubleEveryOther (x1:[]) = [x1]
+doubleEveryOther (x1:x2:xs) = [x1] ++ [2*x2] ++ doubleEveryOther xs
 
 -- Exercise 4 -----------------------------------------
 
 -- Calculate the sum of all the digits in every Integer.
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits [] = 0
+sumDigits (x:xs) = dropLastDigit x + lastDigit x + sumDigits xs
 
 
 -- Exercise 5 -----------------------------------------
 
 -- Validate a credit card number using the above functions.
 luhn :: Integer -> Bool
-luhn = undefined
+luhn n = 0 == mod (sumDigits (doubleEveryOther (toRevDigits n))) 10
 
 -- Exercise 6 -----------------------------------------
 
@@ -42,4 +46,14 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 1 start goal _    = [(start, goal)]
+hanoi n start goal temp = hanoi (n-1) start temp goal ++ 
+                          hanoi 1 start goal temp ++ 
+						  hanoi (n-1) temp goal start
+
+hanoiQ :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoiQ 1 s g _  _  = [(s, g)]
+hanoiQ 2 s g t1 _  = [(s, t1), (s, g), (t1, g)]
+hanoiQ n s g t1 t2 = hanoiQ (n-2) s t2 g t1 ++
+                     hanoiQ 2 s g t1 t2 ++
+					 hanoiQ (n-2) t2 g t1 s
--- week1/HW01Tests.hs
+++ week1/HW01Tests.hs
@@ -24,28 +24,54 @@ ex1Tests = [ Test "lastDigit test" testLastDigit
 
 -- Exercise 2 -----------------------------------------
 
+testToRevDigits :: (Integer, [Integer]) -> Bool
+testToRevDigits (x, xs) = toRevDigits x == xs
+
 ex2Tests :: [Test]
-ex2Tests = []
+ex2Tests = [ Test "toRevDigits test" testToRevDigits
+			 [(123, [3,2,1]), (0, []), (10, [0, 1])]
+		   ]
 
 -- Exercise 3 -----------------------------------------
 
+testDoubleEveryOther :: ([Integer], [Integer]) -> Bool
+testDoubleEveryOther (xs, ys) = ys == doubleEveryOther xs
+
 ex3Tests :: [Test]
-ex3Tests = []
+ex3Tests = [ Test "doubleEveryOther test" testDoubleEveryOther 
+             [([1, 2], [1, 4])]--, ([3, 6, 9], [3, 12, 9]), ([0, 0], [0, 0])] 
+		   ]
 
 -- Exercise 4 -----------------------------------------
 
+testSumDigits :: ([Integer], Integer) -> Bool
+testSumDigits (xs, sum) = sum == sumDigits xs
+
 ex4Tests :: [Test]
-ex4Tests = []
+ex4Tests = [ Test "sumDigits test" testSumDigits
+             [([10,5,18,4], 19), ([], 0)] 
+		   ]
 
 -- Exercise 5 -----------------------------------------
 
+testLuhn :: (Integer, Bool) -> Bool
+testLuhn (n, result) = result == luhn n
+
 ex5Tests :: [Test]
-ex5Tests = []
+ex5Tests = [ Test "luhn Test" testLuhn
+            [(5594589764218858, True), (1234567812345678, False)] 
+           ]
 
 -- Exercise 6 -----------------------------------------
 
+testHanoi :: (Integer, Peg, Peg, Peg, [Move]) -> Bool
+testHanoi (n, a, b, c, ms) = ms == hanoi n a b c
+
 ex6Tests :: [Test]
-ex6Tests = []
+ex6Tests = [ Test "hanoi test" testHanoi
+             [(1, "a", "b", "c", [("a", "b")]),
+			  (2, "x", "y", "z", [("x", "z"), ("x", "y"), ("z", "y")])]
+           ]
 
 -- All Tests ------------------------------------------
 
