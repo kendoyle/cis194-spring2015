3fafc9b7c3f02ed31b1d238e02be938f98cffe35
Kenneth Doyle <ken@kennethdoyle.net>
2015-07-21 23:53:28 -0700

    Add kdoyle week 2 homework.

--- week2/HW02.hs
+++ week2/HW02.hs
@@ -23,38 +23,48 @@ colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
 -- Get the number of exact matches between the actual code and the guess
 exactMatches :: Code -> Code -> Int
-exactMatches = undefined
+exactMatches [] [] = 0
+exactMatches (x:xs) (y:ys)
+    | x == y = 1  + r
+    | otherwise = r
+    where
+        r = exactMatches xs ys
 
 -- Exercise 2 -----------------------------------------
 
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
-countColors = undefined
+countColors ps = map  (\c -> length $ filter (c ==) ps) colors
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches xs ys = foldl (+) 0 (zipWith min (countColors xs) (countColors ys))
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove s c = 
+    let exact = exactMatches s c
+        nonExact = matches s c - exact
+    in Move c exact nonExact
+        
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent (Move guess e n) posSecret = getMove posSecret guess == (Move guess e n)
 
 -- Exercise 5 -----------------------------------------
 
 filterCodes :: Move -> [Code] -> [Code]
-filterCodes = undefined
+filterCodes m cs = filter (isConsistent m) cs
 
 -- Exercise 6 -----------------------------------------
 
 allCodes :: Int -> [Code]
-allCodes = undefined
+allCodes 1 = [[c] | c <- colors]
+allCodes n = [x++y | x <- (allCodes 1), y <- (allCodes (n-1))]
 
 -- Exercise 7 -----------------------------------------
 
