1bbfd6c8784c369997af69f61381958503ed5cfe
Kenneth Doyle <ken@kennethdoyle.net>
2015-08-12 12:17:33 -0700

    Hw5 p7 wip

--- week5/HW05.hs
+++ week5/HW05.hs
@@ -9,42 +9,81 @@ import System.Environment (getArgs)
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Map.Strict as Map
 
+import Data.Bits
+import Data.Function
+
 import Parser
 
 -- Exercise 1 -----------------------------------------
 
 getSecret :: FilePath -> FilePath -> IO ByteString
-getSecret = undefined
+getSecret f1 f2 = do
+    bytes1 <- BS.readFile f1
+    bytes2 <- BS.readFile f2
+    --let secret = (BS.dropWhile (BS.null . BS.singleton) (BS.pack $ BS.zipWith xor bytes1 bytes2))
+    let secret = BS.pack . filter (/=0) $ BS.zipWith xor bytes1 bytes2
+    return secret
 
 -- Exercise 2 -----------------------------------------
 
 decryptWithKey :: ByteString -> FilePath -> IO ()
-decryptWithKey = undefined
+decryptWithKey key path = do
+    cipherBytes <- BS.readFile (path ++ ".enc")
+    let plainText = BS.pack $ BS.zipWith xor cipherBytes (BS.cycle key)
+    BS.writeFile path plainText
 
 -- Exercise 3 -----------------------------------------
 
 parseFile :: FromJSON a => FilePath -> IO (Maybe a)
-parseFile = undefined
+parseFile path = do
+    jsonBytes <- BS.readFile path
+    let decoded = decode jsonBytes
+    return decoded
 
 -- Exercise 4 -----------------------------------------
 
+isVictim :: Maybe [TId] -> Transaction -> Bool
+isVictim Nothing _ = False
+isVictim (Just ids) t = (tid t) `elem` ids
+
+getTransactions :: Maybe[Transaction] -> [Transaction]
+getTransactions Nothing = []
+getTransactions (Just ts) = ts
+
 getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
-getBadTs = undefined
+getBadTs victimFile transactionFile = do
+    victims <- parseFile victimFile :: IO (Maybe[TId])
+    transactions <- parseFile transactionFile :: IO(Maybe[Transaction])
+    let ts = getTransactions transactions
+    let vt = filter (isVictim victims) ts
+    return (Just vt)
 
 -- Exercise 5 -----------------------------------------
 
 getFlow :: [Transaction] -> Map String Integer
-getFlow = undefined
+getFlow ts = 
+    let flowMap = Map.empty
+        getFlow' (t':ts') m = 
+            let fromAmt = Map.findWithDefault 0 (from t') m - (amount t')
+                toAmt = Map.findWithDefault 0 (to t') m + (amount t')
+            in getFlow' ts' . Map.insert (from t') fromAmt $ Map.insert (to t') toAmt m
+        getFlow' [] m = m
+    in getFlow' ts flowMap
 
 -- Exercise 6 -----------------------------------------
 
 getCriminal :: Map String Integer -> String
-getCriminal = undefined
+getCriminal m = head . Map.keys $ Map.filter (== (last $ Map.elems m)) m
 
 -- Exercise 7 -----------------------------------------
 
 undoTs :: Map String Integer -> [TId] -> [Transaction]
-undoTs = undefined
+undoTs flowMap ids = 
+    let payers = reverse . sortBy (compare `on` snd) . Map.toList $ Map.filter (>0) flowMap
+        payees = reverse . sortBy (compare `on` snd) . Map.toList $ Map.filter (<0) flowMap
+        undoTs' (from:froms) (to:tos) (id:ids) = 
+            let amt = minimum [snd from, -1 * snd to] 
+                
 
 -- Exercise 8 -----------------------------------------
 
